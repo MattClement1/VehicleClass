54601ecf640569fa5e74e50dfe5642257347f892
MattClement1 <matthew.clement1@ocsbstudent.ca>
2015-12-02 12:49:01 -0500

    Vehicle Class

---
+++ VehicleClass/VehicleClass.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace VehicleClass
+{
+    partial class frmVehicleClass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewVehicle = new System.Windows.Forms.Button();
+            this.lstVehicleList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnNewVehicle
+            // 
+            this.btnNewVehicle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewVehicle.Location = new System.Drawing.Point(96, 208);
+            this.btnNewVehicle.Name = "btnNewVehicle";
+            this.btnNewVehicle.Size = new System.Drawing.Size(93, 41);
+            this.btnNewVehicle.TabIndex = 0;
+            this.btnNewVehicle.Text = "New Vehicle";
+            this.btnNewVehicle.UseVisualStyleBackColor = true;
+            this.btnNewVehicle.Click += new System.EventHandler(this.btnNewVehicle_Click);
+            // 
+            // lstVehicleList
+            // 
+            this.lstVehicleList.FormattingEnabled = true;
+            this.lstVehicleList.Location = new System.Drawing.Point(12, 12);
+            this.lstVehicleList.Name = "lstVehicleList";
+            this.lstVehicleList.Size = new System.Drawing.Size(260, 173);
+            this.lstVehicleList.TabIndex = 1;
+            // 
+            // frmVehicleClass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstVehicleList);
+            this.Controls.Add(this.btnNewVehicle);
+            this.Name = "frmVehicleClass";
+            this.Text = "Vehicle Class";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewVehicle;
+        private System.Windows.Forms.ListBox lstVehicleList;
+    }
+}
+
---
+++ VehicleClass/VehicleClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VehicleClass
+{
+    public partial class frmVehicleClass : Form
+    {
+
+        class Vehicle
+        {
+            private string _liscensePlate;
+            private string _colour;
+            private int _numOfDoors;
+            private int _speed;
+            private int _maxSpeed;
+
+            public void Accelerate(int speedIncrease)
+            {
+                this._speed = speedIncrease + this._speed;
+            }
+            public void Brake(int speedDecrease)
+            {
+                this._speed = this._speed - speedDecrease;
+            }
+            public string CurrentState()
+            {
+                string returnString;
+
+                returnString = "Speed: " + this._speed + "Liscense Plate: " + this._liscensePlate + 
+                    "# of doors: " + this._numOfDoors + "Colour: " + this._colour;
+
+                return returnString;
+            } 
+        }
+
+        public frmVehicleClass()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewVehicle_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+    }
+}
